250b0816a95b1afd59915b5fc903a55f1b88b4cb
Wolfgang Nagele <mail@wnagele.com>
2025-06-26 14:07:13 +0200

    CNI plugin skeleton

--- cni/cni.go
+++ cni/cni.go
@@ -1,15 +1,59 @@
 package cni
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/spf13/cobra"
+
+	"github.com/containernetworking/cni/pkg/skel"
+	"github.com/containernetworking/cni/pkg/types"
+	type100 "github.com/containernetworking/cni/pkg/types/100"
+	"github.com/containernetworking/cni/pkg/version"
+	bv "github.com/containernetworking/plugins/pkg/utils/buildversion"
 )
+
+type Termination struct {
+	Network string `json:"network"`
+	Via     string `json:"via,omitempty"`
+}
+
+type PluginConf struct {
+	types.PluginConf
+	Id           int           `json:"id"`
+	MTU          int           `json:"mtu,omitempty"`
+	Terminations []Termination `json:"terminations,omitempty"`
+}
 
 func NewCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:   "galactic-cni",
 		Short: "Galactic CNI plugin",
 		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Printf("Running CNI logic with args: %v\n", args)
+			skel.PluginMainFuncs(skel.CNIFuncs{
+				Add: cmdAdd,
+				Del: cmdDel,
+			}, version.All, bv.BuildString("none"))
 		},
 	}
+}
+
+func parseConf(data []byte) (*PluginConf, error) {
+	conf := &PluginConf{}
+	if err := json.Unmarshal(data, &conf); err != nil {
+		return nil, fmt.Errorf("failed to parse")
+	}
+	return conf, nil
+}
+
+func cmdAdd(args *skel.CmdArgs) error {
+	pluginConf, _ := parseConf(args.StdinData)
+	result := &type100.Result{}
+	return types.PrintResult(result, pluginConf.CNIVersion)
+}
+
+func cmdDel(args *skel.CmdArgs) error {
+	pluginConf, _ := parseConf(args.StdinData)
+	result := &type100.Result{}
+	return types.PrintResult(result, pluginConf.CNIVersion)
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,15 @@ module github.com/datum-cloud/galactic
 
 go 1.24
 
-require github.com/spf13/cobra v1.9.1
+require (
+	github.com/containernetworking/cni v1.3.0
+	github.com/containernetworking/plugins v1.7.1
+	github.com/spf13/cobra v1.9.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/vishvananda/netns v0.0.5 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 )
