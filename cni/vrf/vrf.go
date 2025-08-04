facfdaffc18e769eabc2deb64321c41edef8ec6b
Wolfgang Nagele <mail@wnagele.com>
2025-08-04 09:37:21 -0400

    Func to find unused VRF id

--- cni/vrf/vrf.go
+++ cni/vrf/vrf.go
@@ -1,19 +1,31 @@
 package vrf
 
 import (
+	"fmt"
+	"math"
+	"slices"
+
 	"github.com/datum-cloud/galactic/cni/sysctl"
 	"github.com/datum-cloud/galactic/util"
 	"github.com/vishvananda/netlink"
 )
 
+const MinVRFId = uint32(1)
+const MaxVRFId = uint32(math.MaxUint32 - 1)
+
 func Add(id int) error {
 	name := util.GenerateInterfaceNameVRF(id)
+
+	vrfId, err := FindNextAvailableVRFId()
+	if err != nil {
+		return err
+	}
 
 	vrf := &netlink.Vrf{
 		LinkAttrs: netlink.LinkAttrs{
 			Name: name,
 		},
-		Table: uint32(id),
+		Table: uint32(vrfId),
 	}
 
 	if err := netlink.LinkAdd(vrf); err != nil {
@@ -34,4 +46,26 @@ func Delete(id int) error {
 	}
 
 	return netlink.LinkDel(link)
+}
+
+func FindNextAvailableVRFId() (uint32, error) {
+	links, err := netlink.LinkList()
+	if err != nil {
+		return 0, err
+	}
+
+	used := make([]uint32, 0, len(links))
+	for _, link := range links {
+		if vrf, ok := link.(*netlink.Vrf); ok {
+			used = append(used, vrf.Table)
+		}
+	}
+
+	for vrfId := MinVRFId; vrfId <= MaxVRFId; vrfId++ {
+		if !slices.Contains(used, vrfId) {
+			return vrfId, nil
+		}
+	}
+
+	return 0, fmt.Errorf("could not find any available VRF id")
 }
